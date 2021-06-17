8677759965945abfdf202fdbe4239877e91e080f
Pavel <weos@mail.ru>
2021-06-18 02:17:47 +0400

    дз урок 4

---
+++ lesson4/lesson4/Arr.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lesson4
+{
+    /// <summary>
+    /// класс с методами для работы с одномерными массивами
+    /// </summary>
+    static class Arr
+    {
+        /// <summary>
+        /// принимает массив и находит кол-во пар , в которых только одно число делится на 3
+        /// </summary>
+        /// <param name="a">массив целых чисел</param>
+        /// <returns>кол-во</returns>
+        public static int Dn3(int[] a)
+        {
+            int count = 0;
+        for(int i =0; i<a.Length-1;i++)
+            {
+                if ((a[i] % 3 == 0) != (a[i + 1] % 3 == 0))
+                    ++count;
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// функция возвращающая массив всех целых чисел, которые смогла достать из текстового файла
+        /// </summary>
+        /// <param name="filename">путь к нужному txt файлу</param>
+        /// <returns>массив целых чисел</returns>
+        public static int[] outLod(string filename)
+        {
+                int[] a;
+                if (File.Exists(filename))//проверка наличия файла 
+                {
+                        string ss = File.ReadAllText(filename);//переносит весь файл в строку
+                        ss += " ";
+                        a = new int[ss.Length];
+                        int j = 0;
+                        string s="";//одно число
+                        for (int i = 0; i < ss.Length; i++)//просматривает каждый символ
+                        {
+                           try//если символ есть цифра, добавляет его к числу
+                           {
+                             a[j] = int.Parse(Convert.ToString(ss[i]));
+                             s += ss[i];
+                           }
+                           catch (FormatException e)//если символ - не цифра, добавляет текущее число (если оно есть) к массиву
+                           {
+                                if (s != "" && s != "-")
+                                {
+                                    a[j] = int.Parse(s);
+                                    ++j;
+                                }
+                                s = "";
+                                if (ss[i] == '-') s +='-';
+                           }
+                        }
+                        //переносит результат в новый массив, размер которого равен количеству чисел
+                        int[] b = new int[j];
+                        for (int i = 0; i < j; ++i)
+                        b[i] = a[i];
+                        return b;
+                }
+                else
+                throw new FileNotFoundException();
+        }
+
+        public static void output(int[] a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+                Console.Write($"{a[i]} ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lesson4/lesson4/Arr2.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    /// <summary>
+    /// класс для работы с двумерным массивом
+    /// </summary>
+    class Arr2
+    {
+        int[,] a;
+
+        /// <summary>
+        /// конструктор , заполняющий массив случайными числами
+        /// </summary>
+        /// <param name="n">размер первого измерения</param>
+        /// <param name="m">размер второго измерения</param>
+        public Arr2(int n, int m)
+        {
+            a = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = rnd.Next(0, 10);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// конструктор , заполняющий массив из файла
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="filename"></param>
+        public Arr2(int n, int m,string filename)
+        {
+            a = new int[n, m];
+            int[] b= Arr.outLod(filename);
+            int l = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (l < b.Length)
+                    {
+                        a[i, j] = b[l];
+                        ++l;
+                    }
+                    else
+                        a[i, j] = 0;
+
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// возвращает сумму всех элементов массива больше заданного "с"
+        /// </summary>
+        /// <param name="c">минимальное значение для попадания в сумму</param>
+        public int SumMore(int c)
+        {
+            int s = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                   if(a[i,j]>c) s += a[i, j];
+                }
+            }
+            return s;
+        }
+
+        /// <summary>
+        /// возвращает сумму всех элементов массива
+        /// </summary>
+        /// <returns></returns>
+        public int Sum()
+        {
+            return this.SumMore(int.MinValue);
+        }
+
+        public void MaxN(out int n,out int m)
+        {
+            n = 0; m = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (a[i, j] > a[n, m])
+                    {
+                        n = i;m = j;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// выводит двумерный массив в консоль
+        /// </summary>
+        public void output()
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i,j]+"      ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// свойство для изменения и возвращения элементов массива
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public int this[int n,int m]
+        {
+            get
+            {
+                return a[n, m];
+            }
+            set
+            {
+                a[n, m] = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает минимальный элемент массива
+        /// </summary>
+    public int Min
+        {
+            get
+            {
+                int m = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] < m) m = a[i, j];
+                    }
+                }
+                return m;
+            }
+        }
+
+        /// <summary>
+        /// возвращает максимальный элемент массива
+        /// </summary>
+        public int Max
+        {
+            get
+            {
+                int m = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] > m) m = a[i, j];
+                    }
+                }
+                return m;
+            }
+        }
+
+    }
+
+
+}
