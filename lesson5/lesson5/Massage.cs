57509b2be27a8d6e7bcbffcbfb027dbfd1c8933b
Pavel <weos@mail.ru>
2021-06-20 02:25:04 +0400

    дз

---
+++ lesson5/lesson5/Massage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    static class Massage
+    {
+        public static string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+        public static void notMoreN(string ss, int n)
+        {
+            string[] words = ss.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= n)
+                {
+                    Console.Write(words[i]+" ");
+                }
+            }
+        }
+
+        public static string DelEndOfC(string ss, string c)
+        {
+
+            ss=Regex.Replace(ss, @"\b{1}\w*" + c + @"\b{1}","");
+            return ss;
+        }
+
+        public static StringBuilder Leng(string ss)
+        {
+            StringBuilder max = new StringBuilder();
+            string[] words = ss.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > max.Length)
+                {
+                    max.Length = 0;
+                    max.Append(words[i]);
+                }
+                else if(words[i].Length == max.Length)
+                {
+                    max.Append(", " + words[i]);
+                }
+            }
+            return max;
+        }
+
+        public static void Chast(string ss, string[] slo)
+        {
+            Dictionary<string, int> openWith =new Dictionary<string, int>();
+            string[] words = ss.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in slo)
+            {
+                openWith.Add(s, 0);
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                try
+                {
+                    ++openWith[words[i]];
+                }
+                catch (KeyNotFoundException)
+                {
+                }
+            }
+            foreach(string s in slo)
+            {
+                Console.WriteLine(s + "    " + openWith[s]);
+            }
+        }
+    }
+}
---
+++ lesson5/lesson5/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//Павел Тесленко
+namespace lesson5
+{
+    class Program
+    {
+        static string text = @"Стандартный класс string позволяет выполнять над строками различные операции, в том числе поиск, замену, вставку и удаление подстрок.";
+        static void Main(string[] args)
+        {
+            #region задача 1 проверка логина
+            string login;
+                Console.WriteLine("Введите логин от 2 до 10 символов, содержащий только буквы латинского алфавита или цифры, при этом цифра не может быть первой:");
+                login = Console.ReadLine();
+                Console.WriteLine($"Проверка программой без РВ: {log(login)}       с РВ: {log2(login)}");
+                Console.ReadKey();
+                Console.Clear();
+            #endregion
+
+            #region задача 2 класс Message
+            //Вывести только те слова сообщения, которые содержат не более n букв.
+            Console.WriteLine("Исходный текст:\n\n" + text);
+            Console.WriteLine($"\nВведите n, напечатаются только те слова сообщения, которые содержат не более n букв\n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Massage.notMoreN(text, n);
+            Console.ReadKey();
+            Console.Clear();
+
+            //Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+            Console.WriteLine("Исходный текст:\n\n" + text);
+            Console.WriteLine($"\nВведите символ, в тесте будут удалены все слова, заканчивающиеся на него\n");
+            string c = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(Massage.DelEndOfC(text,c));
+            Console.ReadKey();
+            Console.Clear();
+
+            // Найти самое длинное слово сообщения.
+            // Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+            Console.WriteLine("Введите сообщение:\n");
+            Console.WriteLine($"\nСамое длинное слово (слова) сообщения: "+ Massage.Leng(Console.ReadLine()));
+            Console.ReadKey();
+            Console.Clear();
+
+            //Создать метод, который производит частотный анализ текста. В качестве параметра в него передается массив слов и текст, в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст. Здесь требуется использовать класс Dictionary.
+            Console.WriteLine("Исходный текст:\n\n" + text);
+            Console.WriteLine($"\nВведите через пробел слова, для которых нужно найти частоту\n");
+            string[] words = Console.ReadLine().Split(Massage.separators, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine();
+            Massage.Chast(text,words);
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+
+            #region задача 3  Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+            Console.WriteLine("введите первую строку");
+            StringBuilder str1 = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("введите вторую строку");
+            StringBuilder str2 = new StringBuilder(Console.ReadLine());
+            if(Perest(str1,str2)) Console.WriteLine("является перестановкой");
+            else Console.WriteLine("HE является перестановкой");
+            Console.ReadKey();
+            #endregion
+        }
+
+
+        #region к первой задаче
+        /// <summary>
+        /// проверяет соответствие шаблону без РВ
+        /// </summary>
+        /// <param name="ss">строка для проверки</param>
+        /// <returns>тру или фолс соответственно</returns>
+        static bool log(string ss)
+        {
+            if (ss.Length < 2 || ss.Length > 10) return false;
+            for (int i = 0; i < ss.Length; i++)
+            {
+                char s = ss[i];
+                if ((s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') || (i == 0 ? false : (s >= '0' && s <= '9')))
+                    continue;
+                return false;
+            } 
+            return true;
+        }
+
+        /// <summary>
+        /// проверяет соответствие шаблону с РВ
+        /// </summary>
+        /// <param name="ss">строка для проверки</param>
+        /// <returns>тру или фолс соответственно</returns>
+        static bool log2(string ss)
+        {
+            Regex myReg = new Regex("^[a-zA-Z]{1}[a-zA-Z0-9]{1,9}$");
+            return(myReg.IsMatch(ss));
+        }
+        #endregion
+
+        #region ко 2 задаче
+        /// <summary>
+        /// принимает 2 строки и определяет, являются ли они перестановками
+        /// </summary>
+        /// <param name="s1">1 строка</param>
+        /// <param name="s2">2 строка</param>
+        /// <returns>тру если являются, фолс - иначе</returns>
+        static bool Perest(StringBuilder s1, StringBuilder s2)
+        {
+            if (s1.Length != s2.Length) return false;
+                for(int j=0;j<s2.Length;++j)
+                {
+                    if(s1[0]==s2[j])
+                    {
+                        s1.Remove(0, 1);
+                        s2.Remove(j, 1);
+                    j = -1;
+                    }
+                }
+            if (s1.Length == 0) return true;
+            else return false;
+        }
+        #endregion
+
+    }
+}
